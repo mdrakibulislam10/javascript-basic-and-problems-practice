7169e1429f65e8f9185912995e06cbcb48f9cc49
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-02-06 20:05:01 +0600

    public bus fare calculation

---
+++ publicBusFare.js
@@ -0,0 +1,38 @@
+function publicBusFare(travelers) {
+    // bus and microbus capacity
+    const perBusCapacity = 50;
+    const perMicrobusCapacity = 11;
+
+    // public bus fare
+    const publicBusPerTicketFare = 250; // fare = price;
+
+    // rest travelers after fulfil microbus
+    let restTravelersAfterFulfilMicrobus = 0;
+    // public bus fare
+    let restTravelersPublicBusFare = 0;
+
+    if (travelers >= 50) {
+        const restTravelersAfterFulfilBus = travelers % perBusCapacity;
+
+        if (restTravelersAfterFulfilBus >= 11) {
+            restTravelersAfterFulfilMicrobus = restTravelersAfterFulfilBus % perMicrobusCapacity;
+            restTravelersPublicBusFare = restTravelersAfterFulfilMicrobus * publicBusPerTicketFare;
+        }
+        else {
+            restTravelersPublicBusFare = restTravelersAfterFulfilBus * publicBusPerTicketFare;
+        }
+    }
+    else if (travelers >= 11) {
+        restTravelersAfterFulfilMicrobus = travelers % perMicrobusCapacity;
+        restTravelersPublicBusFare = restTravelersAfterFulfilMicrobus * publicBusPerTicketFare;
+    }
+    else {
+        return "Minimum 11 travelers are required";
+    }
+
+    return restTravelersPublicBusFare;
+};
+
+const travelers = 55;
+const publicBusTicketFare = publicBusFare(travelers);
+console.log("public bus fare for rest travelers:", publicBusTicketFare);
